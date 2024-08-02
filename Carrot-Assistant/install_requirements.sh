4136d03b26d2ec743ac972c180bf131a483e0235
Karthikeyan Sivakumar <karthinottsuk@gmail.com>
2024-08-02 10:36:15 +0100

    Updated Commit

--- Carrot-Assistant/install_requirements.sh
+++ Carrot-Assistant/install_requirements.sh
@@ -6,15 +6,11 @@ The script will install the following packages:
 - Latest version of Python.
 
 - Required Python packages from the requirements.txt file.
-
-- Setup OpenSSL to install the openssl package.
 """
 
 
 python --version
 pip install -r requirements.txt
-
-python -c "from config import OpenSSHInstaller; installer = OpenSSHInstaller(); installer.install_openssh()"
 
 
 
