659e77b9d7f610b8ee7c538ebab218cad01c51d7
James Mitchell-White <jamesjimitchell@gmail.com>
2024-09-16 16:14:19 +0100

    Update test_prompt_build.py
    Added test fixtures for prompt builder and a simple test

--- Carrot-Assistant/tests/test_prompt_build.py
+++ Carrot-Assistant/tests/test_prompt_build.py
@@ -6,15 +6,33 @@ def llama_3_simple_prompt_builder():
     return Prompts(
             model_name="llama-3-8b",
             prompt_type="simple",
-            )
+            ).get_prompt()
 
 @pytest.fixture
 def llama_3_rag_prompt_builder():
     return Prompts(
             model_name="llama-3-8b",
             prompt_type="top_n_RAG",
-            )
+            ).get_prompt()
 
 @pytest.fixture
 def llama_3_1_simple_prompt_builder():
-    return Prompts
+    return Prompts(
+            model_name="llama-3.1-8b",
+            prompt_type="simple",
+            eot_token="<|eot_id|>"
+            ).get_prompt()
+
+@pytest.fixture
+def llama_3_1_rag_prompt_builder():
+    return Prompts(
+            model_name="llama-3.1-8b",
+            prompt_type="top_n_RAG",
+            eot_token="<|eot_id|>"
+            ).get_prompt()
+
+def test_simple_prompt_returned(llama_3_simple_prompt_builder):
+    assert "banana" in llama_3_simple_prompt_builder.run(informal_name="banana")["prompt"]
+
+def test_rag_prompt_returned(llama_3_rag_prompt_builder):
+    
