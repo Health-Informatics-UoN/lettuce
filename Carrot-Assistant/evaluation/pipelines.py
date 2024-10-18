e12d3709f1674b72ea4379906acf46ae12185301
James Mitchell-White <jamesjimitchell@gmail.com>
2024-10-18 15:16:49 +0100

    Update pipelines.py
    implemented LLMPipeline.run() to take an input (which has to be a dict so jinja2 can use as a template)

--- Carrot-Assistant/evaluation/pipelines.py
+++ Carrot-Assistant/evaluation/pipelines.py
@@ -1,12 +1,20 @@
+from typing import Dict
 from evaltypes import SingleResultPipeline
 from options.pipeline_options import LLMModel
+from components.models import local_models
 from jinja2 import Template
+from llama_cpp import Llama
+from huggingface_hub import hf_hub_download
 
 
 class LLMPipeline(SingleResultPipeline):
     def __init__(self, llm: LLMModel, prompt_template: Template) -> None:
-        self.llm = (LLMModel,)
+        self.llm = llm
         self.prompt_template = prompt_template
+        self._model = Llama(hf_hub_download(**local_models[self.llm]))
 
-    def run(self, input) -> str:
-        pass
+    def run(self, input: Dict[str, str]) -> str:
+        prompt = self.prompt_template.render(input)
+        return self._model.create_chat_completion(
+            messages=[{"role": "user", "content": prompt}]
+        )["choices"][0]["message"]
