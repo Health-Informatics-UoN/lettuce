ee92a99e6531230ff7af90732cf10d3e1abd3331
James Mitchell-White <jamesjimitchell@gmail.com>
2024-10-29 16:21:54 +0000

    Update metrics.py
    Added the three basic information retrieval metrics: precision, recall, and F-score

--- Carrot-Assistant/evaluation/metrics.py
+++ Carrot-Assistant/evaluation/metrics.py
@@ -1,3 +1,4 @@
+from typing import Any, List
 from evaluation.evaltypes import SingleResultMetric, InformationRetrievalMetric
 from rapidfuzz import fuzz
 
@@ -96,3 +97,128 @@ class FuzzyMatchRatio(SingleResultMetric):
 
 
 # -------- Information Retrieval Metrics --------
+def calc_precision(relevant_instances: List[Any], prediction: List[Any]) -> float:
+    """
+    Compares two lists and calculates precision
+
+    Precision = (Number of relevant instances retrieved)/(Number of instances retrieved)
+
+    Parameters
+    ----------
+    relevant_instances: List[Any]
+        The set of relevant instances, or positive class
+    prediction: List[Any]
+        A prediction made by an information retrieval system
+
+    Returns
+    -------
+    float
+        A score for the precision
+    """
+    relevant_retrieved_instances = [x for x in prediction if x in relevant_instances]
+    return len(relevant_retrieved_instances) / len(prediction)
+
+
+def calc_recall(relevant_instances: List[Any], prediction: List[Any]) -> float:
+    """
+    Compares two lists and calculates recall
+
+    Recall = (Number of relevant instances retrieved)/(Number of relevant instances)
+
+    Parameters
+    ----------
+    relevant_instances: List[Any]
+        The set of relevant instances, or positive class
+    prediction: List[Any]
+        A prediction made by an information retrieval system
+
+    Returns
+    -------
+    float
+        A score for the recall
+    """
+    relevant_retrieved_instances = [x for x in prediction if x in relevant_instances]
+    return len(relevant_retrieved_instances) / len(relevant_instances)
+
+
+class PrecisionMetric(InformationRetrievalMetric):
+    def __init__(self) -> None:
+        self._description = "Precision: a type agnostic precision metric"
+
+    def calculate(self, predicted: List[Any], actual: List[Any]) -> float:
+        """
+        Calculates precision for the information retrieval pipeline's prediction against a positive set
+
+        Parameters
+        ----------
+        predicted: List[Any]
+            The output of an information retrieval pipeline
+        actual: List[Any]
+            The set of relevant instances for the input
+        """
+        return calc_precision(actual, predicted)
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+
+class RecallMetric(InformationRetrievalMetric):
+    def __init__(self) -> None:
+        self._description = "Recall: a type agnostic recall metric"
+
+    def calculate(self, predicted: List[Any], actual: List[Any]) -> float:
+        """
+        Calculates recall for the information retrieval pipeline's prediction against a positive set
+
+        Parameters
+        ----------
+        predicted: List[Any]
+            The output of an information retrieval pipeline
+        actual: List[Any]
+            The set of relevant instances for the input
+        """
+        return calc_recall(actual, predicted)
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+
+class FScoreMetric(InformationRetrievalMetric):
+    def __init__(self, beta: float) -> None:
+        """
+        Initialises the F-Score metric
+
+        Parameters
+        ----------
+        beta: float
+            The ratio by which to weight precision to recall
+        """
+        self._description = (
+            "F Score: a type agnostic F-score metric with a Beta of " + str(beta)
+        )
+        self._beta = beta
+
+    def calculate(self, predicted: List[Any], actual: List[Any]) -> float:
+        """
+        Calculates F score with the class beta for the information retrieval pipeline's prediction against a positive set
+
+        Parameters
+        ----------
+        predicted: List[Any]
+            The output of an information retrieval pipeline
+        actual: List[Any]
+            The set of relevant instances for the input
+        """
+        precision = calc_precision(actual, predicted)
+        recall = calc_recall(actual, predicted)
+        return (
+            (1 + self._beta**2)
+            * (precision * recall)
+            / ((self._beta**2 * precision) + recall)
+        )
+
+    @property
+    def description(self) -> str:
+        return self._description
