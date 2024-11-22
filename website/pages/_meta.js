b06a0a5d8004ae4cfbce7dabea7f899a2aa05545
James Mitchell-White <jamesjimitchell@gmail.com>
2024-11-22 13:20:49 +0000

    Started new docs

---
+++ website/pages/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  index: "Lettuce",
+  api_reference: "Lettuce API reference"
+}
---
+++ website/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>Lettuce documentation</span>,
+  project: {
+    link: 'https://github.com/health-informatics-uon/lettuce'
+  }
+  // ... other theme options
+}
