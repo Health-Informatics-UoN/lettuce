1e9bd2eb08522bde63801e11e337aa77af1e0f72
James Mitchell-White <jamesjimitchell@gmail.com>
2024-11-22 13:24:31 +0000

    set paths for gh actions build

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,11 +1,21 @@
 import nextra from 'nextra'
  
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  basePath: "/lettuce",
+  assetPrefix: "/lettuce",
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  }
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "build"
+}
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
  
-export default withNextra()
- 
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
+export default withNextra(nextConfig)
