a44b456b47d57b718a90f6cf1cb990c46a643fc4
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-05-07 17:08:23 +0100

    Updated and debugged main tests

--- lettuce/tests/e2e/test_cli.py
+++ lettuce/tests/e2e/test_cli.py
@@ -1,7 +1,15 @@
-from pathlib import Path 
 import ast  
 import subprocess 
+import time 
 import pytest 
+
+
+@pytest.fixture(autouse=True, scope="session")
+def time_e2e_session():
+    start = time.time()
+    yield
+    end = time.time()
+    print(f"\nTotal E2E test duration: {end - start:.2f} seconds")
 
 
 def run_lettuce_cli_command(cli_args: list[str]): 
@@ -44,10 +52,11 @@ def test_basic_search():
         "acetaminophen"
     ])
     output = parse_output(result.stdout)
+    omop_matches = output[0]["OMOP matches"]["CONCEPT"]
     assert result.returncode == 0 
     assert len(output) > 0 
-    assert any(match["concept_name"] == "acetaminophen" for match in output)
-    assert all(match["vocabulary_id"] == "RxNorm" for match in output)
+    assert any(match["concept_name"] == "acetaminophen" for match in omop_matches)
+    assert all(match["vocabulary_id"] == "RxNorm" for match in omop_matches)
 
 
 def test_vector_search(): 
@@ -73,20 +82,37 @@ def test_vector_search():
 
 def test_llm_pipeline(): 
     result = run_lettuce_cli_command([
-        "--vector_search", 
+        "--no-vector_search", 
         "--use_llm", 
         "--vocabulary_id", 
         "RxNorm", 
+        "--model_name", 
+        "tinyllama-1.1b-chat", 
         "--informal_names", 
         "acetaminophen"
     ])
     output = parse_output(result.stdout)
-    breakpoint()
+    assert len(output) > 0
+    assert output[0]["llm_answer"] == "Acetaminophen"
 
 
 def test_combined_vector_and_llm(): 
-    pass 
-
-
-if __name__ == "__main__": 
-    pass 
+    result = run_lettuce_cli_command([
+        "--vector_search", 
+        "--use_llm", 
+        "--vocabulary_id", 
+        "RxNorm", 
+        "--model_name", 
+        "tinyllama-1.1b-chat", 
+        "--informal_names", 
+        "acetaminophen"
+    ])
+    output = parse_output(result.stdout)
+    omop_matches = output[0]["OMOP matches"]["CONCEPT"]
+    vector_search_results = output[0]["Vector Search Results"]
+    assert result.returncode == 0 
+    assert len(output) > 0 
+    assert len(omop_matches) > 0 
+    assert output[0]["llm_answer"] == "Acetaminophen"
+    assert len(vector_search_results) > 0 
+    assert any(match["content"] == "Acetaminophen" for match in vector_search_results)
