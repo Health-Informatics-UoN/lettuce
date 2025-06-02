71bc4b852083441faff76fe1cf4af8311e64b8f7
James Mitchell-White <jamesjimitchell@gmail.com>
2025-06-02 20:14:58 +0100

    Set up classes for API

---
+++ lettuce/api/responses.py
@@ -0,0 +1,22 @@
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+
+class Suggestion(BaseModel):
+    concept_name: str
+    concept_id: int
+    domain_id: str
+    vocabulary_id: str
+    concept_class_id: str
+    standard_concept: Optional[str]
+    invalid_reason: Optional[str]
+    ranks: Optional[Dict[str, int]]
+    scores: Optional[Dict[str, float]]
+
+class SuggestionsMetaData(BaseModel):
+    assistant: str = "Lettuce"
+    version: str = "0.1.0"
+    pipeline: str
+
+class ConceptSuggestionResponse(BaseModel):
+    recommendations: List[Suggestion]
+    metadata: SuggestionsMetaData
