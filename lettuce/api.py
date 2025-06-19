c263b87cb4b6152ffda47d32e473c22f41ce9294
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-06-19 08:58:43 +0100

    Added verify_api_key function to app.py entrypoint

--- lettuce/api.py
+++ lettuce/api.py
@@ -1,6 +1,27 @@
-from fastapi import FastAPI
+import os 
+from fastapi import FastAPI, Depends, HTTPException, status  
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
 from routers import pipeline_routes, search_routes
+
+
+security = HTTPBearer()
+
+def verify_api_key(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    api_key = os.getenv("AUTH_API_KEY")
+    if not api_key:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server configuration error: API_KEY not set"
+        )
+    valid_api_keys = {api_key}
+    if credentials.credentials not in valid_api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API key"
+        )
+    return credentials.credentials
+
 
 app = FastAPI(
     title="OMOP concept Assistant",
@@ -23,12 +44,14 @@ app.add_middleware(
 app.include_router(
     router=pipeline_routes.router,
     prefix="/pipeline",
+    dependencies=[Depends(verify_api_key)]  
 )
 
 app.include_router(
-        router=search_routes.router,
-        prefix="/search",
-        )
+    router=search_routes.router,
+    prefix="/search",
+    dependencies=[Depends(verify_api_key)]  
+)
 
 
 def main():
