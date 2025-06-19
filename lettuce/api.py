66ae970cabc49c5448a8990e566d9a2b4c5710f7
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-06-19 13:13:06 +0100

    Added unit tests

--- lettuce/api.py
+++ lettuce/api.py
@@ -1,9 +1,12 @@
 import os 
+from dotenv import load_dotenv
 from fastapi import FastAPI, Depends, HTTPException, status  
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
 from routers import pipeline_routes, search_routes
 
+
+load_dotenv()
 
 security = HTTPBearer()
 
