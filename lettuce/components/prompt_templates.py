bf6a442cb0c957746decfcbb9251425fc6564645
James Mitchell-White <jamesjimitchell@gmail.com>
2025-09-15 11:17:07 +0100

    Added domain description to prompt + test

--- lettuce/components/prompt_templates.py
+++ lettuce/components/prompt_templates.py
@@ -1,5 +1,19 @@
 templates = {
-        "simple": """You are an assistant that suggests formal OMOP concept names for a {{domain}}. Respond only with the formal name of that {{domain}}, without any extra explanation.
+        "simple": """You are an assistant that suggests formal OMOP concept names for a source term. Respond only with the formal name of that {% if domain %}
+    {%- if domain|length == 1 -%}
+    {{ domain[0] }}
+    {%- else -%}
+        {%- for d in domain -%}
+         {% if not loop.last -%}
+            {{d}}, 
+         {% else -%}
+            or {{d}}
+         {%- endif -%}
+        {%- endfor -%}
+    {%- endif -%}
+    {%- else -%}
+    source term
+{%- endif -%}, without any extra explanation.
 
 Examples:
 
@@ -18,9 +32,22 @@ Response: Naproxen
 Task:
 
 Informal name: {{informal_name}}""",
-            "top_n_RAG": """You are an assistant that suggests formal OMOP concept names for a {{domain}}. You will be given the name of a {{domain}}, along with some possibly related OMOP concept names. If you do not think these terms are related, ignore them when making your suggestion.
+            "top_n_RAG": """You are an assistant that suggests formal OMOP concept names for source terms. You will be given the name of a {% if domain %}
+    {%- if domain|length == 1 -%}
+    {{ domain[0] }}
+    {%- else -%}
+        {%- for d in domain -%}
+         {% if not loop.last -%}
+            {{d}}, {% else -%}
+            or {{d}}
+         {%- endif -%}
+        {%- endfor -%}
+    {%- endif -%}
+    {%- else -%}
+    source term
+{%- endif -%}, along with some possibly related OMOP concept names. If you do not think these terms are related, ignore them when making your suggestion.
 
-Respond only with the formal name of the {{domain}}, without any extra explanation.
+Respond only with the formal name of the source term, without any extra explanation.
 
 Examples:
 
@@ -44,5 +71,4 @@ Possible related terms:
 Task:
 
 Informal name: {{informal_name}}""",
-
         }
