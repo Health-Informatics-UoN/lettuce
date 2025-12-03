3d2d12a05c6f5373ac146e6e014be33eb17579cd
James Mitchell-White <jamesjimitchell@gmail.com>
2025-12-03 11:44:45 +0000

    add method for accepting from SuggestionRecord

--- lettuce-ui/src/suggestions/__init__.py
+++ lettuce-ui/src/suggestions/__init__.py
@@ -11,16 +11,6 @@ class ConceptSuggestion:
     score: float = 0.0
 
 @dataclass
-class SuggestionRecord:
-    search_term: str
-    domains: List[str]
-    vocabs: List[str]
-    standard_concept: bool
-    valid_concept: bool
-    search_mode: Literal["text-search", "vector-search", "ai-search"]
-    suggestion: ConceptSuggestion
-
-@dataclass
 class AcceptedSuggestion:
     search_term: str
     domains: List[str]
@@ -34,3 +24,44 @@ class AcceptedSuggestion:
     vocabulary_id: str
     standard_concept: str
     score: float = 0.0
+    
+@dataclass
+class SuggestionRecord:
+    search_term: str
+    domains: List[str]
+    vocabs: List[str]
+    standard_concept: bool
+    valid_concept: bool
+    search_mode: Literal["text-search", "vector-search", "ai-search"]
+    suggestion: List[ConceptSuggestion]
+
+    def accept_suggestion(self, index: int) -> AcceptedSuggestion:
+        """
+        Select one of the suggestions and make an AcceptedSuggestion out of it 
+
+        Parameters
+        ----------
+        index: int
+            The index of the accepted suggestion
+
+        Returns
+        -------
+        AcceptedSuggestion
+            An AcceptedSuggestion with the details of this record and the chosen suggestion
+        """
+
+        return AcceptedSuggestion(
+                search_term=self.search_term,
+                domains=self.domains,
+                vocabs=self.vocabs,
+                search_standard_concept=self.standard_concept,
+                valid_concept=self.valid_concept,
+                search_mode=self.search_mode,
+                concept_id=self.suggestion[index].concept_id,
+                concept_name=self.suggestion[index].concept_name,
+                domain_id=self.suggestion[index].domain_id,
+                vocabulary_id=self.suggestion[index].vocabulary_id,
+                standard_concept=self.suggestion[index].standard_concept,
+                score=self.suggestion[index].score,
+                )
+
