dac90e3f243f444ca74d437ff2fd85af335e4eef
James Mitchell-White <jamesjimitchell@gmail.com>
2026-02-18 10:38:10 +0000

    Create embedder.py

---
+++ build-embeddings/embedding_utils/embedder.py
@@ -0,0 +1,31 @@
+from sentence_transformers import SentenceTransformer
+from jinja2 import Template
+
+from embedding_utils.protocols import ConceptEmbedder, EmbeddedConcept
+from embedding_utils.string_building import Concept
+
+class BatchEmbedder(ConceptEmbedder):
+    def __init__(
+            self,
+            embedding_model: SentenceTransformer,
+            template: Template,
+            ) -> None:
+        super().__init__()
+        self._embedding_model = embedding_model
+        self._template = template
+    
+    def embed_concepts(self, concepts: list[Concept]) -> list[EmbeddedConcept]:
+        concept_strings = [c.render_concept_as_template(self._template) for c in concepts]
+        concept_embeddings = self._embedding_model.encode(
+                [c.concept_string for c in concept_strings],
+                convert_to_tensor=False,
+                show_progress_bar=True
+                ).tolist()
+        return list([
+            EmbeddedConcept(
+                concept_id=concept.concept_id,
+                concept_name=concept.concept_name,
+                embedding=embedding
+                )
+            for concept, embedding in zip(concept_strings, concept_embeddings)
+            ])
