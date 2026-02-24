9dc4dc3a6f828e42967d69936aa47d4453d00789
James Mitchell-White <jamesjimitchell@gmail.com>
2026-02-24 16:04:39 +0000

    added ConceptExtractor for csv

--- build-embeddings/embedding_utils/concept_readers.py
+++ build-embeddings/embedding_utils/concept_readers.py
@@ -1,11 +1,27 @@
 from collections.abc import Generator
 from logging import Logger
+from pathlib import Path
 
 from psycopg import sql
+import polars as pl
 
 from embedding_utils.string_building import Concept
 from embedding_utils.protocols import ConceptReader
 from embedding_utils.db_utils import PGConnector
+
+# Polars' schema inference doesn't work well on Athena's vocab files, so it needs an explicit schema
+CONCEPT_SCHEMA = {
+        "concept_id": pl.Int32(),
+        "concept_name": pl.String(),
+        "domain_id": pl.String(),
+        "vocabulary_id": pl.String(),
+        "concept_class_id": pl.String(),
+        "standard_concept": pl.String(),
+        "concept_code": pl.String(),
+        "valid_start_date": pl.String(),
+        "valid_end_date": pl.String(),
+        "invalid_reason": pl.String(),
+        }
 
 class PostgresConceptExtractor(ConceptReader):
     """
@@ -80,3 +96,47 @@ class PostgresConceptExtractor(ConceptReader):
                                     concept_class=r[4]
                                     ) for r in rows
                                 ]
+
+class CsvConceptExtractor(ConceptReader):
+    """
+    A ConceptReader that connects to a CSV file in the Athena standard format and reads the concepts.
+
+    Attributes
+    ----------
+    path: Path
+        The path to a CSV file of concepts
+    table_schema: dict[str, pl.DataType]
+        A schema for the concept table
+    """
+    def __init__(
+            self,
+            path: Path,
+            table_schema: dict[str, pl.DataType] = CONCEPT_SCHEMA
+            ) -> None:
+        self._path = path
+        self._table_schema = table_schema
+
+    def load_concept_batch(self, batch_size: int) -> Generator[list[Concept]]:
+        concept_df = pl.scan_csv(self._path, schema=self._table_schema, separator='\t', quote_char=None)
+        for batch in concept_df.collect_batches(chunk_size=batch_size):
+            yield [
+                    Concept(
+                        concept_id=r["concept_id"],
+                        concept_name=r["concept_name"],
+                        domain=r["domain"],
+                        vocabulary=r["vocabulary_id"],
+                        concept_class=r["concept_class_id"]
+                        ) for r in batch.iter_rows(named=True)
+                    ]
+
+    def load_concepts(self) -> list[Concept]:
+        concept_df = pl.read_csv(self._path, schema=self._table_schema, separator='\t', quote_char=None)
+        return [
+                Concept(
+                    concept_id=r["concept_id"],
+                    concept_name=r["concept_name"],
+                    domain=r["domain"],
+                    vocabulary=r["vocabulary_id"],
+                    concept_class=r["concept_class_id"]
+                    ) for r in concept_df.iter_rows(named=True)
+                ]
