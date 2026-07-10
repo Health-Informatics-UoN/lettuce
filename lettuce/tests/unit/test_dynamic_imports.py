61fad3f915c2a2d1da7278ec8b344f619308772d
James Mitchell-White <jamesjimitchell@gmail.com>
2026-07-10 12:11:10 +0100

    just test for local_model import

--- lettuce/tests/unit/test_dynamic_imports.py
+++ lettuce/tests/unit/test_dynamic_imports.py
@@ -17,7 +17,12 @@ def test_llama_loaded():
 def test_llama_not_loaded():
     from components.models import get_model
 
-    llama_cpp_modules = [x for x in sys.modules.keys() if "llama_cpp" in x]
-    print(llama_cpp_modules)
-
-    assert(len(llama_cpp_modules) == 0)
+    # Unfortunately, haystack pulls in llama_cpp stuff if it's installed,
+    # whether you like it or not. Obviously this is a good design for them,
+    # but it's inconvenient, as this test doesn't work
+    # llama_cpp_modules = [x for x in sys.modules.keys() if "llama_cpp" in x]
+    # print(llama_cpp_modules)
+    #
+    # assert(len(llama_cpp_modules) == 0)
+    # and we just have to test this instead:
+    assert "components.models.local_models" not in sys.modules
